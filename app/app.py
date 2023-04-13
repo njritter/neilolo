9fbe03f1ac580181c551e0abcc38b179c78f940a
njritter <ritter.neil@gmail.com>
2023-04-13 09:54:12 -0600

    Add home html page

--- app/app.py
+++ app/app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>I love you, Lolo! Test 1</p>"
+    return render_template("home.html")
+
+
+@app.route("/page2")
+def hello_world2():
+    return "<p>I love you, Lolo! Page 2</p>"
