3164d10f0c7be8b890e19cb1dce85eef406977b9
njritter <ritter.neil@gmail.com>
2023-04-18 12:38:33 -0600

    continued photo playing

--- app/app.py
+++ app/app.py
@@ -2,14 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
+def home():
     return render_template("home.html")
+
 
 @app.route("/blog")
 def blog():
     return "<p>I love you, Lolo! Page 2</p>"
 
+
 @app.route("/wedding")
 def wedding():
-    return "<p>I love you, Lolo! Page 2</p>"
+    return render_template("wedding.html")
